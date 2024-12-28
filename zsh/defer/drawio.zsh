fd51376667871c729d6b57c033541458b18056cd
a4rcvv <35623957+a4rcvv@users.noreply.github.com>
2024-12-28 15:54:57 +0900

    fix

--- zsh/defer/drawio.zsh
+++ zsh/defer/drawio.zsh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # skip if drawio is not available
-DRAWIO_PATH = "/Applications/draw.io.app/Contents/MacOS/draw.io"
+DRAWIO_PATH="/Applications/draw.io.app/Contents/MacOS/draw.io"
 if [ ! -f "${DRAWIO_PATH}" ]; then
     return
 fi
